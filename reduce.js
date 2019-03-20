1f18bad5d1d085dfced74eef12e1172a8c7083a9
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2019-03-20 18:28:44 +0200

    added reduce

--- reduce.js
+++ reduce.js
@@ -1,3 +1,11 @@
-function reduce(arr, cb, initialValue) {}
+function reduce(arr, cb, initialValue) {
+  let result = initialValue;
+
+  for (let i = 0; i < arr.length; i++) {
+    result = cb(result, arr[i]);
+  }
+
+  return result;
+}
 
 module.exports = reduce;
