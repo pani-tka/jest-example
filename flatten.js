09f1279219720fe24498fc7bde9c619de242fc62
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2019-03-21 15:45:07 +0200

    Removed comments and added another test

--- flatten.js
+++ flatten.js
@@ -3,39 +3,4 @@ const flatten = arr =>
     return previousValue.concat(currentValue);
   }, []);
 
-const arr = [1, 2, 3, 4, 5];
-
-const sum = arr.reduce(function(acc, elem) {
-  return acc + elem;
-}, 0);
-
-const sumValue = sum(arr);
-
-/**
- * Step 0:
- *
- * acc = 0;
- * elem = 1;
- *
- * Step 1:
- *
- * acc = 1
- * elem = 2
- *
- * Step 2:
- *
- * acc = 3
- * elem = 3
- *
- * Step 3:
- *
- * acc = 6
- * elem = 4
- *
- * Step 4:
- *
- * acc = 10
- * elem = 5
- */
-
 module.exports = flatten;
--- flatten.test.js
+++ flatten.test.js
@@ -4,5 +4,12 @@ test('Empty array', () => {
   const actualValue = flatten([]);
   const expectedValue = [];
 
-  expect(actualValue).toBeEqual(expectedValue);
+  expect(actualValue).toEqual(expectedValue);
+});
+
+test('Book case', () => { 
+  const actualValue = flatten([[1, 2, 3], [4, 5], [6]]);
+  const expectedValue = [1, 2, 3, 4, 5, 6];
+
+  expect(actualValue).toEqual(expectedValue);
 });
